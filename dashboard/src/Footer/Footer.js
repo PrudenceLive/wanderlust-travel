1bf2892133f79b65797b0844e2985ff01eaec647
prudencelive <129216286+PrudenceLive@users.noreply.github.com>
2024-05-30 12:41:13 +0200

    Create Footer.js

---
+++ dashboard/src/Footer/Footer.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <footer>
+      <div className="container">
+        <div className="footer-par">
+          <div className="footer-1">
+            <h3 className="footer-logo-h">Wanderlust-Travel</h3>
+          </div>
+          <div className="footer-1 footer-info">
+            <h3>Find Your Happy Place</h3>
+          </div>
+        </div>
+      </div>
+      <p className="copyright">Copyright © 2024 Created By Prudence Maseko</p>
+    </footer>
+  );
+}
+
+export default Footer;
